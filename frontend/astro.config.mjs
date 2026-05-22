69324c1950158fdf7b8e8620ee640a9fce1e411c
adri <adriamm.97@gmail.com>
2026-05-22 09:55:05 +0200

    Refactor home data structure and update assets
    - Updated home data in data.json to include a new hero image and reorganized carrousel items.
    - Removed unused assets and added new images for the carrousel.
    - Modified HeroSection component to accept dynamic image and alt text.
    - Updated SiteHeader to use an SVG logo instead of text spans for better accessibility and responsiveness.
    - Changed formApi to use environment variable for access key.
    - Adjusted index.astro to reflect changes in data structure and pass hero image to HeroSection.

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://whiterDJ-adri.github.io",
+	base: "/paula-sWeb",
 	vite: {
 		plugins: [tailwindcss()],
 	},
-
 });
